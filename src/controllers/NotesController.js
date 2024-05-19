f358590ee794b69dc51a41ed303e3b4062c1e469
unknown <alexandresoaresrodrigues2004@gmail.com>
2024-05-19 17:06:46 -0300

    adição de arquivos novos

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -3,7 +3,11 @@ const knex = require("../database/knex")
 class NotesController {
   async create(request, response) {
     const { title, description, rating, tags} = request.body
-    const { user_id } = request.params
+    const user_id = request.user.id;
+
+    if (rating < 0 || rating > 5) {
+      throw new AppError("A nota deve ser entre 0 e 5.");
+  }
 
     const [note_id] = await knex("movie_notes").insert({
       title,
@@ -47,7 +51,9 @@ async delete(request, response) {
 }
 
 async index(request, response) {
-  const { title, user_id, tags } = request.query
+  const { title, tags } = request.query
+
+  const user_id = request.user.id;
 
   let notes
 
--- src/server.js
+++ src/server.js
@@ -1,13 +1,17 @@
 require('express-async-errors')
 const database = require('./database/sqlite')
 const AppError = require('./utils/AppError')
+const uploadConfig = require("./configs/upload");
 
 const express = require('express')
-
+const cors = require("cors");
 const routes = require('./routes')
 
 const app = express()
+app.use(cors());
 app.use(express.json())
+
+app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER));
 
 app.use(routes)
 
